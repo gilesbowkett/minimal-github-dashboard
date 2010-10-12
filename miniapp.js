f7bddc0e0631930a9b01749e6cfa388144d8077c
Giles Bowkett <gilesb@gmail.com>
2010-10-11 21:07:18 -0700

    now it works for everybody

--- miniapp.js
+++ miniapp.js
@@ -4,14 +4,17 @@ var http = require('http'),
     jade = require('./jade');
 
 http.createServer(function (req, res) {
-  rest.get("http://github.com/api/v2/json/repos/show/gilesbowkett", {
-    data: {},
-  }).addListener('complete', function(data, github_response) {
-    if (github_response.statusCode == 200) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      jade.renderFile('view.jade', { locals: {data: data} }, function(err, html){
-        res.end(html);
-      });
-    }
-  });
+  var github_username = req.url.replace(/\//, "");
+  if ("favicon.ico" != github_username) {
+    rest.get("http://github.com/api/v2/json/repos/show/" + github_username, {
+      data: {},
+    }).addListener('complete', function(data, github_response) {
+      if (github_response.statusCode == 200) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        jade.renderFile('view.jade', { locals: {data: data} }, function(err, html){
+          res.end(html);
+        });
+      }
+    });
+  }
 }).listen(3000, "127.0.0.1");
