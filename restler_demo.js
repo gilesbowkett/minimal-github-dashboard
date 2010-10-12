b8f8cbfe05fae596dbda826dbfd81b5192d202ab
Giles Bowkett <gilesb@gmail.com>
2010-10-11 19:53:33 -0700

    finally, a templating solution that doesn't suck

--- restler_demo.js
+++ restler_demo.js
@@ -1,17 +1,17 @@
 var http = require('http'),
     sys = require('sys'),
     rest = require('./restler/lib/restler'),
-    Mu = require('./mu/lib/mu');
+    jade = require('./jade');
 
 http.createServer(function (req, res) {
   rest.get("http://gilesbowkett:Vclg0'$akj@github.com/api/v2/json/repos/show/gilesbowkett", {
     data: {},
   }).addListener('complete', function(data, github_response) {
     if (github_response.statusCode == 200) {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      var template = "Hello fool. Your name is: {{name}}!", partials = {}, compiled = Mu.compileText(template, partials);
-      compiled({data: data}).addListener('data', function (c) { res.write(c); res.write(JSON.stringify(data)); res.end("fuck yeah"); });
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      jade.renderFile('view.html', { options: '' }, function(err, html){
+        res.end(html);
+      });
     }
   });
 }).listen(3000, "127.0.0.1");
-
